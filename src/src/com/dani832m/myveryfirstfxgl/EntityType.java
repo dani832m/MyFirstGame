c364e1c9b8977c8f7d16301f5b04da6564839674
Daniel <31951102+dani832m@users.noreply.github.com>
2018-02-09 01:20:32 +0100

    Delete EntityType.java

--- src/src/com/dani832m/myveryfirstfxgl/EntityType.java
+++
@@ -1,9 +0,0 @@
-//Angiver hvilken package kildekoden er placeret i
-package com.dani832m.myveryfirstfxgl;
-
-/**
- * Enum, der definerer hvilke enhedestyper, der findes i spillet.
- */
-public enum EntityType {
-    PLAYER, SAUSAGE
-}
