a301594a48399219d92b435487f893f479dde6d9
Daniel <31951102+dani832m@users.noreply.github.com>
2018-02-09 01:10:58 +0100

    Kildekode til mit spil v.1.1

---
+++ src/src/com/dani832m/myveryfirstfxgl/EntityType.java
@@ -0,0 +1,9 @@
+//Angiver hvilken package kildekoden er placeret i
+package com.dani832m.myveryfirstfxgl;
+
+/**
+ * Enum, der definerer hvilke enhedestyper, der findes i spillet.
+ */
+public enum EntityType {
+    PLAYER, SAUSAGE
+}
